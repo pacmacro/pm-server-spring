d7574574942e7a7a547d4d877aeff35f8a23097a
jleung51 <jleung513@gmail.com>
2016-06-02 23:17:48 -0700

    Implement Coordinate type

---
+++ pm-server/src/main/java/com/pm/server/datatype/Coordinate.java
@@ -0,0 +1,13 @@
+package com.pm.server.datatype;
+
+public interface Coordinate {
+
+	void setLatitude(double latitude);
+
+	double getLatitude();
+
+	void setLongitude(double longitude);
+
+	double getLongitude();
+
+}
---
+++ pm-server/src/main/java/com/pm/server/datatype/CoordinateImpl.java
@@ -0,0 +1,24 @@
+package com.pm.server.datatype;
+
+public class CoordinateImpl implements Coordinate {
+
+	double latitude;
+	double longitude;
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+}
