0437770ae178e3dc5da590b5a7d374c93e2b82fe
jleung51 <jleung513@gmail.com>
2016-06-06 02:11:57 -0700

    Add getAllLocations() to ghost controller

--- pm-server/src/main/java/com/pm/server/controller/GhostController.java
+++ pm-server/src/main/java/com/pm/server/controller/GhostController.java
@@ -1,7 +1,14 @@
 package com.pm.server.controller;
 
+import java.util.Map;
+
+import com.pm.server.datatype.Coordinate;
+
 public interface GhostController extends Controller, PlayerController {
 
 	public int getInteger();
+
+	// Returns map of ghost id to location
+	public Map<Integer, Coordinate> getAllLocations();
 
 }
--- pm-server/src/main/java/com/pm/server/controller/GhostControllerImpl.java
+++ pm-server/src/main/java/com/pm/server/controller/GhostControllerImpl.java
@@ -1,12 +1,24 @@
 package com.pm.server.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pm.server.datatype.Coordinate;
+import com.pm.server.player.Ghost;
+import com.pm.server.player.GhostRepository;
+
 @RestController
 @RequestMapping("/ghost")
 public class GhostControllerImpl implements GhostController {
+
+	@Autowired
+	private GhostRepository ghostRepository;
 
 	@Override
 	@RequestMapping(
@@ -15,6 +27,26 @@ public class GhostControllerImpl implements GhostController {
 	)
 	public int getInteger() {
 		return 0;
+	}
+
+	// Returns map of ghost id to location
+	@Override
+	@RequestMapping(
+			value="/locations",
+			method=RequestMethod.GET
+	)
+	public Map<Integer, Coordinate> getAllLocations() {
+
+		List<Ghost> ghosts = ghostRepository.getAllGhosts();
+
+		Map<Integer, Coordinate> ghostsLocations =
+				new HashMap<Integer, Coordinate>();
+
+		for(Ghost ghost : ghosts) {
+			ghostsLocations.put(ghost.getId(), ghost.getLocation());
+		}
+
+		return ghostsLocations;
 	}
 
 }
