ff12f62d3a115fef32544ba440cdec6bbc307c64
jleung51 <jleung513@gmail.com>
2016-06-06 01:51:35 -0700

    Remove excess import
    NO WARNINGS ALLOWED

--- pm-server/src/main/java/com/pm/server/player/GhostRepositoryImpl.java
+++ pm-server/src/main/java/com/pm/server/player/GhostRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.pm.server.player;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
