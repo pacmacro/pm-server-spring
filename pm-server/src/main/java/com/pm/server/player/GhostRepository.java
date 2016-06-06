ebada944c81d374a821a56e0fc3d240f8a1064b9
jleung51 <jleung513@gmail.com>
2016-06-06 01:48:01 -0700

    Add GhostRepository base class/interface

---
+++ pm-server/src/main/java/com/pm/server/player/GhostRepository.java
@@ -0,0 +1,17 @@
+package com.pm.server.player;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GhostRepository {
+
+	// Returns null if the ghost with the corresponding id is not found
+	Ghost getGhostById(int id);
+
+	List<Ghost> getAllGhosts();
+
+	void addGhost(Ghost ghost) throws Exception;
+
+}
---
+++ pm-server/src/main/java/com/pm/server/player/GhostRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.pm.server.player;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GhostRepositoryImpl implements GhostRepository {
+
+	List<Ghost> ghosts;
+
+	public Ghost getGhostById(int id) {
+
+		for(Ghost ghost : ghosts) {
+			if(ghost.getId() == id) {
+				return ghost;
+			}
+		}
+		return null;
+
+	}
+
+	public List<Ghost> getAllGhosts() {
+		return ghosts;
+	}
+
+	public void addGhost(Ghost ghost) throws Exception {
+
+		if(getGhostById(ghost.getId()) != null) {
+			ghosts.add(ghost);
+		}
+		else {
+			throw new IllegalArgumentException(
+				"addGhost() was given an id belonging to a ghost which is " +
+				"already in the registry."
+			);
+		}
+
+	}
+
+}
