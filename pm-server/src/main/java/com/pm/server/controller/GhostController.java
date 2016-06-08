502779656b89483da3c2d2163da5f27527384e1d
jleung51 <jleung513@gmail.com>
2016-06-07 22:02:59 -0700

    Implement /ghost/{id}/location getLocationById mapping

--- pm-server/src/main/java/com/pm/server/controller/GhostController.java
+++ pm-server/src/main/java/com/pm/server/controller/GhostController.java
@@ -1,8 +1,16 @@
 package com.pm.server.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
+import com.pm.server.response.GhostResponse;
 import com.pm.server.response.GhostsResponse;
 
 public interface GhostController extends Controller, PlayerController {
+
+	public GhostResponse getLocationById(
+			Integer id,
+			HttpServletResponse response
+	);
 
 	public GhostsResponse getAllLocations();
 
--- pm-server/src/main/java/com/pm/server/controller/GhostControllerImpl.java
+++ pm-server/src/main/java/com/pm/server/controller/GhostControllerImpl.java
@@ -2,7 +2,10 @@ package com.pm.server.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,32 @@ public class GhostControllerImpl implements GhostController {
 
 	@Autowired
 	private GhostRepository ghostRepository;
+
+	@RequestMapping(
+			value="/{id}/location",
+			method=RequestMethod.GET,
+			produces={ "application/json" }
+	)
+	public GhostResponse getLocationById(
+			@PathVariable Integer id,
+			HttpServletResponse response) {
+
+		if(id == null) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return null;
+		}
+
+		Ghost ghost = ghostRepository.getGhostById(id);
+		if(ghost == null) {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return null;
+		}
+
+		GhostResponse ghostResponse = new GhostResponse();
+		ghostResponse.setId(ghost.getId());
+		ghostResponse.setLocation(ghost.getLocation());
+		return ghostResponse;
+	}
 
 	@RequestMapping(
 			value="/locations",
