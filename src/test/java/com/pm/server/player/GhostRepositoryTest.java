a2b4fcd7162f3685fa63c0c1381821cc52007fa0
jleung51 <jleung513@gmail.com>
2016-06-18 14:37:33 -0700

    Add and set up 3 ghosts for GhostRepositoryTest

--- src/test/java/com/pm/server/player/GhostRepositoryTest.java
+++ src/test/java/com/pm/server/player/GhostRepositoryTest.java
@@ -13,9 +13,16 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.pm.server.TestTemplate;
+import com.pm.server.datatype.Coordinate;
 import com.pm.server.datatype.CoordinateImpl;
 
 public class GhostRepositoryTest extends TestTemplate {
+
+	private Ghost ghost1;
+
+	private Ghost ghost2;
+
+	private Ghost ghost3;
 
 	@Autowired
 	private GhostRepository ghostRepository;
@@ -28,8 +35,25 @@ public class GhostRepositoryTest extends TestTemplate {
 	@Before
 	public void setUp() {
 
-		List<Ghost> ghostList = ghostRepository.getAllGhosts();
+		Integer ghost1_id = 12345;
+		Coordinate ghost1_location = new CoordinateImpl(1.0, 0.1);
+		ghost1 = new GhostImpl();
+		ghost1.setId(ghost1_id);
+		ghost1.setLocation(ghost1_location);
 
+		Integer ghost2_id = 23456;
+		Coordinate ghost2_location = new CoordinateImpl(2.0, 0.2);
+		ghost2 = new GhostImpl();
+		ghost2.setId(ghost2_id);
+		ghost2.setLocation(ghost2_location);
+
+		Integer ghost3_id = 34567;
+		Coordinate ghost3_location = new CoordinateImpl(3.0, 0.3);
+		ghost3 = new GhostImpl();
+		ghost3.setId(ghost3_id);
+		ghost3.setLocation(ghost3_location);
+
+		List<Ghost> ghostList = ghostRepository.getAllGhosts();
 		for(Ghost ghost : ghostList) {
 			try {
 				ghostRepository.deleteGhostById(ghost.getId());
@@ -39,7 +63,6 @@ public class GhostRepositoryTest extends TestTemplate {
 				fail();
 			}
 		}
-
 		assertTrue(ghostRepository.getAllGhosts().isEmpty());
 
 	}
