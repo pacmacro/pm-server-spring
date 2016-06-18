4d31634cf27553dbd378ff4418cf81bee85756b4
jleung51 <jleung513@gmail.com>
2016-06-17 20:35:39 -0700

    Add Ghost repository method to set location

--- src/main/java/com/pm/server/player/GhostRepository.java
+++ src/main/java/com/pm/server/player/GhostRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.pm.server.datatype.Coordinate;
+
 @Repository
 public interface GhostRepository {
 
@@ -11,6 +13,8 @@ public interface GhostRepository {
 	Ghost getGhostById(Integer id);
 
 	List<Ghost> getAllGhosts();
+
+	void setGhostLocationById(Integer id, Coordinate location);
 
 	void addGhost(Ghost ghost) throws Exception;
 
