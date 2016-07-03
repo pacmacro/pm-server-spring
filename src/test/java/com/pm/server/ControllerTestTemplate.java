a11801f23b869a1f0d42f53e9804a3f438d40490
jleung51 <jleung513@gmail.com>
2016-07-03 10:03:40 -0700

    Write unit test for Home ("/") controller

---
+++ src/test/java/com/pm/server/ControllerTestTemplate.java
@@ -0,0 +1,8 @@
+package com.pm.server;
+
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+public abstract class ControllerTestTemplate extends TestTemplate {
+
+}
---
+++ src/test/java/com/pm/server/controller/HomeControllerTest.java
@@ -0,0 +1,47 @@
+package com.pm.server.controller;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.pm.server.ControllerTestTemplate;
+
+public class HomeControllerTest extends ControllerTestTemplate {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(this.webApplicationContext)
+				.build();
+
+	}
+
+	@Test
+	public void unitTest_home() throws Exception {
+
+		// Given
+		String path = "/";
+
+		// When
+		mockMvc
+				.perform(get(path))
+
+		// Then
+				.andExpect(status().isOk());
+
+	}
+
+}
