7ceb8ef87129e2db4e63a06d8e26b96e9fe8ba58
jleung51 <jleung513@gmail.com>
2016-07-04 21:39:54 -0700

    Add skeleton for GhostControllerTest

---
+++ src/test/java/com/pm/server/controller/GhostControllerTest.java
@@ -0,0 +1,66 @@
+package com.pm.server.controller;
+
+import static org.junit.Assert.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.pm.server.ControllerTestTemplate;
+import com.pm.server.datatype.Coordinate;
+import com.pm.server.datatype.CoordinateImpl;
+public class GhostControllerTest extends ControllerTestTemplate {
+
+	private static final List<Coordinate> randomCoordinateList = Arrays.asList(
+			new CoordinateImpl(12345.54321, 95837.39821),
+			new CoordinateImpl(49381.30982, 39399.49932)
+	);
+
+	private static final String BASE_MAPPING = "/ghost";
+
+	private static final Logger log =
+			LogManager.getLogger(GhostControllerTest.class.getName());
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(this.webApplicationContext)
+				.build();
+
+	}
+
+	private void createGhost_failUponException(Coordinate location) {
+
+		String path =
+				BASE_MAPPING + "/" +
+				location.getLatitude() + "/" +
+				location.getLongitude();
+
+		try {
+			mockMvc
+					.perform(post(path))
+					.andExpect(status().isOk());
+		}
+		catch(Exception e) {
+			log.error(e.getMessage());
+			fail();
+		}
+
+	}
+
+}
