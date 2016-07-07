be969580a902724630019e4ebd875aae1fb9ae65
jleung51 <jleung513@gmail.com>
2016-07-06 21:53:08 -0700

    Improve createGhost GhostControllerTest to return id

--- src/test/java/com/pm/server/controller/GhostControllerTest.java
+++ src/test/java/com/pm/server/controller/GhostControllerTest.java
@@ -1,5 +1,6 @@
 package com.pm.server.controller;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,9 +13,11 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.jayway.jsonpath.JsonPath;
 import com.pm.server.ControllerTestTemplate;
 import com.pm.server.datatype.Coordinate;
 import com.pm.server.datatype.CoordinateImpl;
@@ -44,22 +47,29 @@ public class GhostControllerTest extends ControllerTestTemplate {
 
 	}
 
-	private void createGhost_failUponException(Coordinate location) {
+	// Returns the ID of the created ghost
+	private Integer createGhost_failUponException(Coordinate location) {
 
 		String path =
 				BASE_MAPPING + "/" +
 				location.getLatitude() + "/" +
 				location.getLongitude();
+		String jsonContent = null;
 
 		try {
-			mockMvc
+			MvcResult result = mockMvc
 					.perform(post(path))
-					.andExpect(status().isOk());
+					.andExpect(status().isOk())
+					.andReturn();
+			jsonContent = result.getResponse().getContentAsString();
 		}
 		catch(Exception e) {
 			log.error(e.getMessage());
 			fail();
 		}
+
+		assertNotNull(jsonContent);
+		return JsonPath.read(jsonContent, "$.id");
 
 	}
 
