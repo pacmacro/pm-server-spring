e894107796baa686507c991a20cfc2d281305843
jleung51 <jleung513@gmail.com>
2016-07-16 23:16:39 -0700

    Sweeten the Heroku logs monitoring script
    It's not exactly syntactic sugar, but it'll do.

--- api-calls/monitoring-scripts/heroku_logs.sh
+++ api-calls/monitoring-scripts/heroku_logs.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+echo ""
 echo "The logs are restricted to developers only."
 echo "If you are a developer but you are not authorized to access these logs, please contact GitHub user jleung51."
+echo "If the last line is 'Process exited', the server is most likely asleep. Call any endpoint to wake the server up."
 echo ""
+echo "Starting in 5 seconds..."
+echo ""
+
+sleep 5s
 
 heroku logs \
   --tail --force-colors \
