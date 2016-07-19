cd4e18ef3612c09623b7db1554585315d18b31ad
jleung51 <jleung513@gmail.com>
2016-07-18 21:41:32 -0700

    Revert "Add link to specification in README"
    This reverts commit 76d46fc390eb30d81f7cbe7871782ce575a03060.
    
    Files were added which I didn't want to add just yet.

--- api-calls/utility/ghost_states.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Valid states for a Ghost are:"
-echo "  UNINITIALIZED"
-echo "  READY"
-echo "  ACTIVE"
-echo "  CAPTURED"
--- api-calls/utility/pacman_states.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Valid states for the Pacman are:"
-echo "  UNINITIALIZED"
-echo "  READY"
-echo "  ACTIVE"
-echo "  CAPTURED"
-echo "  POWERUP"
