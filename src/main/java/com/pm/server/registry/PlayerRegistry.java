8b7d88b963ae8a54ed011e1f6f6cf8ba1cf3cf4e
jleung51 <jleung513@gmail.com>
2016-07-23 23:21:33 -0700

    Add PlayerRegistry as PlayerRepo wrapper
    This enforces the rules, with 4 Ghosts and a Pacman.
    
    The Controller(s) will interface with this instead of the GhostRepository directly.

---
+++ src/main/java/com/pm/server/registry/PlayerRegistry.java
@@ -0,0 +1,23 @@
+package com.pm.server.registry;
+
+import java.util.List;
+
+import com.pm.server.datatype.Coordinate;
+import com.pm.server.datatype.PlayerName;
+import com.pm.server.datatype.PlayerState;
+import com.pm.server.player.Player;
+
+public interface PlayerRegistry {
+
+	// Returns null if the player with the corresponding id is not found
+	Player getPlayerByName(PlayerName name);
+
+	List<Player> getAllPlayers();
+
+	void setPlayerLocationByName(PlayerName name, Coordinate location);
+
+	void setPlayerStateByName(PlayerName name, PlayerState state);
+
+	void reset() throws Exception;
+
+}
