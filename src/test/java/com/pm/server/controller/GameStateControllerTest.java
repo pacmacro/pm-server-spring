c58d6ec3e0f2abeb5bd0874f666711e158d02fff
jleung51 <jleung513@gmail.com>
2016-09-03 00:58:24 -0700

    Write tests for the gamestate controller

--- src/test/java/com/pm/server/controller/GameStateControllerTest.java
+++ src/test/java/com/pm/server/controller/GameStateControllerTest.java
@@ -1,5 +1,8 @@
 package com.pm.server.controller;
+
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,8 +15,14 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.pm.server.ControllerTestTemplate;
 import com.pm.server.game.GameState;
+import com.pm.server.registry.GameStateRegistry;
+import com.pm.server.request.StringStateContainer;
+import com.pm.server.utils.JsonUtils;
 
 public class GameStateControllerTest extends ControllerTestTemplate {
+
+	@Autowired
+	private GameStateRegistry gameStateRegistry;
 
 	private static final String BASE_MAPPING = "/gamestate";
 
@@ -28,6 +37,10 @@ public class GameStateControllerTest extends ControllerTestTemplate {
 		mockMvc = MockMvcBuilders
 				.webAppContextSetup(this.webApplicationContext)
 				.build();
+
+		if(gameStateRegistry.getCurrentState() != GameState.INITIALIZING) {
+			gameStateRegistry.resetGame();
+		}
 
 	}
 
@@ -48,7 +61,502 @@ public class GameStateControllerTest extends ControllerTestTemplate {
 
 	}
 
+	@Test
+	public void unitTest_putGameState_noState() throws Exception {
+
+		// Given
+		final String path = pathForPutGameState();
+
+		// When
+		mockMvc
+				.perform(put(path)
+				)
+
+		// Then
+				.andExpect(status().isBadRequest());
+
+	}
+
+	@Test
+	public void unitTest_putGameState_invalidState() throws Exception {
+
+		// Given
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState("Invalid state");
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isBadRequest());
+
+	}
+
+	@Test
+	public void unitTest_resetGame() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		assertEquals(
+				GameState.IN_PROGRESS,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.INITIALIZING.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isOk());
+
+		assertEquals(
+				GameState.INITIALIZING,
+				gameStateRegistry.getCurrentState()
+		);
+
+	}
+
+	@Test
+	public void unitTest_resetGame_uninitialized() throws Exception {
+
+		// Given
+		assertEquals(
+				GameState.INITIALIZING,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.INITIALIZING.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_startGame() throws Exception {
+
+		// Given
+		assertEquals(
+				GameState.INITIALIZING,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.IN_PROGRESS.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isOk());
+
+		assertEquals(
+				GameState.IN_PROGRESS,
+				gameStateRegistry.getCurrentState()
+		);
+
+	}
+
+	@Test
+	public void unitTest_startGame_fromPaused() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		gameStateRegistry.pauseGame();
+		assertEquals(
+				GameState.PAUSED,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.IN_PROGRESS.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isOk());
+
+		assertEquals(
+				GameState.IN_PROGRESS,
+				gameStateRegistry.getCurrentState()
+		);
+
+	}
+
+	@Test
+	public void unitTest_startGame_inProgress() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		assertEquals(
+				GameState.IN_PROGRESS,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.IN_PROGRESS.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_startGame_winner() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		gameStateRegistry.setWinnerGhosts();
+		assertEquals(
+				GameState.FINISHED_GHOSTS_WIN,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.IN_PROGRESS.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_pauseGame() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		assertEquals(
+				GameState.IN_PROGRESS,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.PAUSED.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isOk());
+
+		assertEquals(
+				GameState.PAUSED,
+				gameStateRegistry.getCurrentState()
+		);
+
+	}
+
+	@Test
+	public void unitTest_pauseGame_initializing() throws Exception {
+
+		// Given
+		assertEquals(
+				GameState.INITIALIZING,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.PAUSED.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_pauseGame_paused() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		gameStateRegistry.pauseGame();
+		assertEquals(
+				GameState.PAUSED,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.PAUSED.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_pauseGame_winner() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		gameStateRegistry.setWinnerGhosts();		
+		assertEquals(
+				GameState.FINISHED_GHOSTS_WIN,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.PAUSED.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_setWinnerGhosts() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		assertEquals(
+				GameState.IN_PROGRESS,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.FINISHED_GHOSTS_WIN.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isOk());
+
+		assertEquals(
+				GameState.FINISHED_GHOSTS_WIN,
+				gameStateRegistry.getCurrentState()
+		);
+
+	}
+
+	@Test
+	public void unitTest_setWinnerPacman() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		assertEquals(
+				GameState.IN_PROGRESS,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.FINISHED_PACMAN_WIN.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isOk());
+
+		assertEquals(
+				GameState.FINISHED_PACMAN_WIN,
+				gameStateRegistry.getCurrentState()
+		);
+
+	}
+
+	@Test
+	public void unitTest_setWinner_uninitialized() throws Exception {
+
+		// Given
+		assertEquals(
+				GameState.INITIALIZING,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.FINISHED_PACMAN_WIN.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_setWinner_paused() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		gameStateRegistry.pauseGame();
+		assertEquals(
+				GameState.PAUSED,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.FINISHED_PACMAN_WIN.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
+	@Test
+	public void unitTest_setWinner_currentWinner() throws Exception {
+
+		// Given
+		gameStateRegistry.startGame();
+		gameStateRegistry.setWinnerGhosts();
+		assertEquals(
+				GameState.FINISHED_GHOSTS_WIN,
+				gameStateRegistry.getCurrentState()
+		);
+
+		final String path = pathForPutGameState();
+
+		StringStateContainer state = new StringStateContainer();
+		state.setState(GameState.FINISHED_GHOSTS_WIN.toString());
+		final String body = JsonUtils.objectToJson(state);
+
+		// When
+		mockMvc
+				.perform(put(path)
+						.content(body)
+						.header("Content-Type", "application/json")
+				)
+
+		// Then
+				.andExpect(status().isConflict());
+
+	}
+
 	private String pathForGetGameState() {
+		return BASE_MAPPING;
+	}
+
+	private String pathForPutGameState() {
 		return BASE_MAPPING;
 	}
 
