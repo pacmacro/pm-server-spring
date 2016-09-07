aa5c25798fb160957d023fe5d967b9c609cbd6ee
Jeffrey Leung <jleung513@gmail.com>
2016-09-07 13:53:27 -0700

    Add game state & score monitoring scripts

---
+++ api-calls/monitoring-scripts/game_score.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $(dirname $0)
+base_url=$(../utility/base_url.sh)
+
+watch \
+  --no-title \
+  --interval 1 "
+  curl \
+    --silent \
+    --request GET \
+    -H "Content-Type: application/json" \
+    $base_url/pacdots/score \
+  | python -m json.tool
+"
