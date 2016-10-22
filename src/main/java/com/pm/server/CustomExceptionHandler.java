526fe80630195d5780865f15895d0bf084d90580
jleung51 <jleung513@gmail.com>
2016-10-22 02:05:22 -0700

    Log outgoing HTTP errors

--- src/main/java/com/pm/server/CustomExceptionHandler.java
+++ src/main/java/com/pm/server/CustomExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.pm.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,9 +10,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+	private final static Logger log =
+			LogManager.getLogger(CustomExceptionHandler.class.getName());
+
 	@ExceptionHandler(value = {PmServerException.class})
 	public ResponseEntity<PmErrorResponse>
 	pmServerExceptionHandler(PmServerException e) {
+
+		HttpStatus status = e.getStatus();
+		String logMessage = 
+				"HTTP " + status.value() + " " + status.getReasonPhrase() +
+				": " + e.getMessage();
+
+		if(status.is5xxServerError()) {
+			log.error(logMessage);
+		}
+		else {
+			log.debug(logMessage);
+		}
 
 		PmErrorResponse response = new PmErrorResponse();
 
