1ed1e19b24d0a3d5d2a9b78f952862abdda48cee
jleung51 <jleung513@gmail.com>
2016-10-27 00:03:23 -0700

    Remove extra API script help text

--- api-calls/player/put_player_location.sh
+++ api-calls/player/put_player_location.sh
@@ -3,7 +3,6 @@
 cd $(dirname $0)
 base_url=$(../utility/base_url.sh)
 player_names=$(../utility/player_names.sh)
-player_states=$(../utility/player_states.sh)
 
 if [ $# -ge 3 ] ; then
     curl \
@@ -16,7 +15,5 @@ else
     echo "Usage: ./put_player_location.sh name latitude longitude"
     echo ""
     echo "$player_names"
-    echo ""
-    echo "$player_states"
     echo ""
 fi
