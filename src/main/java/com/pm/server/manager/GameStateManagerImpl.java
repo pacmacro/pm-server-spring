b89f6b5bbbcf4eb43f241be696eb0bdda9a0c9d3
Jeffrey Leung <jleung513@gmail.com>
2017-09-03 01:58:29 -0700

    Add eaten Ghosts to game score

--- src/main/java/com/pm/server/manager/GameStateManagerImpl.java
+++ src/main/java/com/pm/server/manager/GameStateManagerImpl.java
@@ -4,6 +4,7 @@ import com.pm.server.datatype.GameState;
 import com.pm.server.datatype.Pacdot;
 import com.pm.server.registry.GameStateRegistry;
 import com.pm.server.registry.PacdotRegistry;
+import com.pm.server.registry.PlayerRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +15,22 @@ public class GameStateManagerImpl implements GameStateManager {
 
     private GameStateRegistry gameStateRegistry;
     private PacdotRegistry pacdotRegistry;
+    private PlayerRegistry playerRegistry;
 
     @Autowired
     public GameStateManagerImpl(
             GameStateRegistry gameStateRegistry,
-            PacdotRegistry pacdotRegistry) {
+            PacdotRegistry pacdotRegistry,
+            PlayerRegistry playerRegistry) {
         this.gameStateRegistry = gameStateRegistry;
         this.pacdotRegistry = pacdotRegistry;
+        this.playerRegistry = playerRegistry;
     }
 
     @Override
     public Integer getScore() {
         Integer score = 0;
+
         List<Pacdot> pacdotList = pacdotRegistry.getAllPacdots();
         for(Pacdot pacdot : pacdotList) {
             if(pacdot.getEaten()) {
@@ -37,6 +42,9 @@ public class GameStateManagerImpl implements GameStateManager {
                 }
             }
         }
+
+        score += playerRegistry.getCapturedGhosts() * 50;
+
         return score;
     }
 
