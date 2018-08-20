7a4491fa122c23131c199f2081594dbfe0d7129c
jleung51 <jleung513@gmail.com>
2018-08-20 02:58:23 -0700

    Try again to add CORS header

--- src/main/java/com/pm/server/PmServerConfiguration.java
+++ src/main/java/com/pm/server/PmServerConfiguration.java
@@ -14,7 +14,7 @@ public class PmServerConfiguration {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
