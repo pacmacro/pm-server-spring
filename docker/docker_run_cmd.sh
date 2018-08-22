817df572e051537485a77c867f28c9749746cef0
jleung51 <jleung513@gmail.com>
2018-08-21 17:29:05 -0700

    Add Dockerfile printing when running image

--- docker/docker_run_cmd.sh
+++ docker/docker_run_cmd.sh
@@ -2,5 +2,23 @@
 
 cd $(dirname $0)/..
 
+echo ""
+echo "___________________________________________________________"
+echo ""
+echo "  This Docker image is generated from the following repositories:"
+echo "    https://github.com/pacmacro/pm-server"
+echo "    https://hub.docker.com/r/pacmacro/pm-server/"
+echo ""
+echo "__________________"
+echo ""
+echo "  The Dockerfile is as follows:"
+echo ""
+cat Dockerfile
+echo ""
+echo "___________________________________________________________"
+echo ""
+
 git pull
+
 mvn spring-boot:run
+
